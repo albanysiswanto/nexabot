684b9051d593322fa067decfdf9abc0ba80d6c56
Albany Siswanto <111110704+albanysiswanto@users.noreply.github.com>
2025-05-21 07:23:19 +0700

    Create endpoint ping

--- bot.py
+++ bot.py
@@ -2,12 +2,29 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from flask import Flask
+import threading
 
 load_dotenv()
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive!"
+
+@app.route("/ping")
+def flask_ping():
+    return "Ping received from UptimeRobot!"
+
+def run_flask():
+    host = os.getenv("HOST", "0.0.0.0")
+    port = int(os.getenv("PORT", 5050))
+    app.run(host=host, port=port)
+
 intents = discord.Intents.default()
-# intents.message_content = True
-# intents.members = True
+intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -19,9 +36,11 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("Pong!")
 
-token = os.getenv("DISCORD_BOT_TOKEN")
+flask_thread = threading.Thread(target=run_flask)
+flask_thread.start()
 
-if token is None:
+token = os.getenv("DISCORD_BOT_TOKEN")
+if not token:
     raise ValueError("Environment variable DISCORD_BOT_TOKEN belum di-set!")
 
 bot.run(token)
